ac6ce9299535dcde41d1d067c1e4108c4302d2ab
diogo-ms <diogomarquessimoes@gmail.com>
2023-07-19 15:42:01 +0200

    feat: calculator is working but only one operation at a time

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
 let firstNum = 0;
 let secondNum = 0;
-let displayNum = 0;
+let currentOperator = null;
+let displayValue = null;
 
 const buttons = document.querySelectorAll(".digits");
 const displayElement = document.querySelector(".display");
-let displayValue = null;
+const operators = document.querySelectorAll('.operators');
+const equalButton = document.getElementById('equals');
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    updateDisplay(button.textContent);
+  })
+});
+operators.forEach(button => {
+  button.addEventListener("click", () => {
+    firstNum = displayValue
+    currentOperator = button.textContent
+  })
+})
+equalButton.addEventListener("click", () => {
+  operate(firstNum, displayValue, currentOperator);
+})
 
 function add(a, b) {
-  return a + b;
+  let result = a +b;
+  return updateDisplay(result);
 }
 function subtract(a, b) {
   return a - b;
@@ -22,7 +40,7 @@ function operate(a, b, operator) {
   a = Number(a);
   b = Number(b);
   switch (operator) {
-    case "plus":
+    case "+":
       return add(a, b);
     case "subtract":
       return subtract(a, b);
@@ -35,12 +53,7 @@ function operate(a, b, operator) {
       return null;
   }
 }
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    updateDisplay(button);
-  })
-});
-function updateDisplay(element) {
-  displayElement.textContent = element.textContent;
+function updateDisplay(text) {
+  displayElement.textContent = text;
   displayValue = displayElement.textContent;
 }
